5be77323492fa64af3f0de6be28a61e28eb34022
slaellec <laellec@user-Latitude-E5400.(none)>
2012-12-19 17:19:02 +0100

    Signed-off-by: slaellec <laellec@user-Latitude-E5400.(none)>

--- Lanceur.java
+++
@@ -1,9 +0,0 @@
-public class Lanceur
-{
-	public static void main(String[] args)
-	{
-		System.out.println("Le programme commence ici");
-		// Il faudra surement faire plusieurs fonctions et objets : affichage, aléatoire , ...
-		// Lalala
-	}
-}
