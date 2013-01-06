d86d08694aefa05c77b8161ba971a22b52128081
bigjulien <julienleboss04@hotmail.fr>
2013-01-06 13:01:52 +0100

    Signed-off-by: bigjulien <julienleboss04@hotmail.fr>

--- MemoryPanel.java
+++ MemoryPanel.java
@@ -1,9 +1,6 @@
 import java.awt.*;
 import java.awt.event.*;
-import java.awt.event.MouseEvent;
-import java.awt.event.MouseListener;
-import java.awt.event.MouseMotionListener;
- 
+
 
 
 public class MemoryPanel extends Panel implements MouseListener{ 
@@ -65,33 +62,29 @@ public class MemoryPanel extends Panel implements MouseListener{
 		{
 			add(tc[i]);
 		}
-			
+		addMouseListener(this);		
 		setBackground(Color.black);
-		this.addMouseListener(this);		
+			
 	}
-	@Override
-	public void mouseClicked(MouseEvent e) {}
 
-	@Override
-	public void mouseEntered(MouseEvent e) {}
-
-	@Override
-	public void mouseExited(MouseEvent e) {}
-	
-	@Override
-	public void mouseReleased(MouseEvent e) {}
-	
-	@Override
-	public void mousePressed(MouseEvent e) 
-	{
-		int quellecarte ;
-		System.out.println("souris");
+	public void mousePressed(MouseEvent e) {		
+		int quellecarte ;		
 		int x = (int)(e.getX()/largeurCard);
 		int y = (int)(e.getY()/largeurCard);		
 		quellecarte = (y*nbCaseL)+x;
-		
+		System.out.println(quellecarte);
 		tc[quellecarte].cachee = false;
-		this.repaint();
-	}
+		repaint();}
+
+	
+	public void mouseEntered(MouseEvent e) {}
+
+	
+	public void mouseExited(MouseEvent e) {}
+	
+	
+	public void mouseReleased(MouseEvent e) {}
+	
+	public void mouseClicked(MouseEvent e) {}
 	
 }
